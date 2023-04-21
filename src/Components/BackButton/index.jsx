aaf21bf375eddcdb0fc7e59f6645af0e31c1b709
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-21 18:08:57 -0300

    create component backbutton

---
+++ src/Components/BackButton/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+import { FiArrowLeft } from "react-icons/fi";
+
+export function BackButton() {
+  return (
+    <Container>
+      <FiArrowLeft /> Voltar
+    </Container>
+  )
+}
---
+++ src/Components/BackButton/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  background: none;
+  border: none;
+
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  color: ${({ theme }) => theme.COLORS.GREEN};
+`
