9764d51c91a4cb8f75b25991dcea7ddcbb96f6a7
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-21 17:04:49 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
+
 import { ThemeProvider } from 'styled-components'
-import { Details } from "./pages/Details"
-import theme from './pages/styles/theme'
 import GlobalStyle from './pages/styles/global'
+import { Preview } from "./pages/Preview"
+import theme from './pages/styles/theme'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Details />
+      <Preview />
     </ThemeProvider>
   </React.StrictMode>
 )
