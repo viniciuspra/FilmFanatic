f96ffafc63bf22274716f37c50b23449683ebab1
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-21 18:09:59 -0300

    Update Preview page

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from './pages/styles/global'
-import { Preview } from "./pages/Preview"
+import { Profile } from "./pages/Profile"
 import theme from './pages/styles/theme'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Preview />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/Preview/styles.js
+++ src/pages/Preview/styles.js
@@ -4,13 +4,47 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
 
-  display: grid;
-  grid-template-rows: 10.5rem auto;
-  grid-template-areas:
-    "header"
-    "content";
-
   > main {
     padding: 4rem 14rem 15rem;
+
+    > div {
+      margin-bottom: 2.4rem;
+    }
+
+    > p {
+      font-size: 1.6rem;
+      text-align: justify;
+      font-weight: normal;
+    }
+
+  }
+
+
+`
+
+export const InfoBox = styled.div`
+  width: 100%;
+
+  > div {
+    display: flex;
+    align-items: baseline;
+    gap: 1rem;
+    margin: 2.4rem 0;
+  }
+
+  > span {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    margin: 2.4rem 0;
+
+    > img {
+      width: 1.6rem;
+      border-radius: 50%;
+    }
+
+    > svg {
+      color: ${({ theme }) => theme.COLORS.GREEN};
+    }
   }
 `
