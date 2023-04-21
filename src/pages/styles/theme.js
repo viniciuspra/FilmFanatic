716a96ea83a32d5e431ad04fd5a27b7d20b77e22
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-21 18:10:20 -0300

    Update theme.js

--- src/pages/styles/theme.js
+++ src/pages/styles/theme.js
@@ -3,6 +3,8 @@ export default {
     BACKGROUND_900: "#1C1B1E",
     BACKGROUND_800: "#262529",
     BACKGROUND_700: "#312E38",
+    BACKGROUND_TAG: "#282124",
+
 
     BLACK: "#0D0C0F",
     WHITE: "#F4EDE8",
