701bbc50941a2b813d7beb2eedd88e297eb07f3c
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-21 15:42:58 -0300

    Create theme.js

---
+++ src/pages/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#1C1B1E",
+    BACKGROUND_800: "#262529",
+    BACKGROUND_700: "#312E38",
+
+    BLACK: "#0D0C0F",
+    WHITE: "#F4EDE8",
+    GREEN: "#B4D42A",
+
+    GRAY_100: "#948F99",
+    GRAY_300: "#666360",
+
+  },
+}
