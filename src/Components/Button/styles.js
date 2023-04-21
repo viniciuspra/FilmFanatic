55f0c8706e743d92adfd43596fccdad742b547a6
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-21 18:09:05 -0300

    create component button

---
+++ src/Components/Button/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  width: 100%;
+  background-color: ${({theme}) => theme.COLORS.GREEN};
+  color: ${({theme}) => theme.COLORS.BACKGROUND_800};
+
+  height: 56px;
+  border: 0;
+  padding: 0 16px;
+  margin-top: 16px;
+  border-radius: 10px;
+  font-weight: 500;
+
+  transition: all .2s;
+
+  &:disabled {
+    opacity: 0.5;
+  }
+
+  &:hover {
+    transform: scale(0.99);
+  }
+
+  &:active {
+    transform: scale(0.96);
+  }
+`; 
