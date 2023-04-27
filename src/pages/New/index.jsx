c572b6590cb0ad5061466b3e8a65acb4d8c1819a
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-04-27 15:41:08 -0300

    Create Page New

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,43 @@
+import { Container, Form, Section } from "./styles"; 
+import { Input } from "../../Components/Input";
+import { Button } from "../../Components/Button";
+import { Header } from "../../Components/Header";
+import { TagItem } from "../../Components/TagItem";
+import { TextArea } from "../../Components/TextArea";
+import { BackButton } from "../../Components/BackButton";
+import { MaskedInput } from "../../Components/MaskedInput";
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <header>
+          <BackButton />
+          <h1>Novo filme</h1>
+        </header>
+        <Form>
+          <Section>
+            <Input placeholder="Título" type="text" />
+            <MaskedInput placeholder="Sua nota (de 0 a 5)" />
+            <TextArea placeholder="Descrição" />
+          </Section>
+
+          <h1>Marcadores</h1>
+
+          <div className="Mark">
+            <TagItem value="Família" />
+            <TagItem isNew placeholder="Novo marcador" />
+          </div>
+
+          <div className="buttons">
+            <Button title="Excluir filme" isNew/>
+            <Button title="Salvar alterações" />
+          </div>
+
+        </Form>
+      </main>
+    </Container>
+  )
+}
---
+++ src/pages/New/styles.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  > main {
+    width: 80vw;
+    margin: 38px auto;
+    overflow-y: auto;
+    
+    > header {
+      display: flex;
+      flex-direction: column;
+      justify-content: left;
+      gap: 2.4rem;
+
+      margin-bottom: 4rem;
+    }
+  }
+`
+
+export const Form = styled.form`
+  width: 100%;
+
+  > h1 {
+    font-size: 2rem;
+    font-weight: 400;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+
+    margin: 4rem 0 2.4rem;
+  }
+
+  .Mark {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: left;
+    align-items: center;
+    gap: 1.6rem;
+    
+    background-color: ${({ theme }) => theme.COLORS.BLACK};
+
+    padding: 1.6rem;
+
+    border-radius: 1rem;
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: space-between;
+    gap: 2rem;
+
+    margin: 4rem 0 10rem;
+  }
+
+`
+
+export const Section = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1.6rem;
+`
