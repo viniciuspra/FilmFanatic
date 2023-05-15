2a83e7d3bd1412781aeb4df7804b95ae13ee68a5
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-05-15 19:12:01 -0300

    Implement user registration and API integration in signup page

--- src/pages/SingUp/index.jsx
+++ src/pages/SingUp/index.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
 import { FiUser, FiMail, FiLock, FiArrowLeft } from "react-icons/fi"
 import { Button } from "../../Components/Button"
 import { Input } from "../../Components/Input";
 import { Link } from "react-router-dom";
+import { api } from "../../services/api";
 
 export function SingUp() {
+  const [ name, setName ] = useState("");
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+
+  function handleSignUp() {
+    if(!name || !email || !password) {
+      return alert("Preencha todos os campos!");
+    }
+
+    api.post("/users", {name, email, password})
+      .then(() => {
+        alert("Usuário cadastrado com sucesso!");
+      })
+      .catch(error => {
+        if(error.response) {
+          alert(error.response.data.message);
+        }else {
+          alert("Não foi possível cadastar")
+        }
+      });
+
+  }
+
   return (
     <Container>
       <Form>
@@ -13,13 +38,31 @@ export function SingUp() {
 
         <h2>Crie sua conta</h2>
 
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Button title="Cadastrar" />
+        <Button title="Cadastrar" onClick={handleSignUp} />
 
-        <Link to="/"> <FiArrowLeft/> Voltar para o login</Link>
+        <Link to="/">
+          {" "}
+          <FiArrowLeft /> Voltar para o login
+        </Link>
       </Form>
       <Background />
     </Container>
