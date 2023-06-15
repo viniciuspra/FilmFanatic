84c6478099046e75c76b235c89ec73a62d7475e7
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-06-15 01:01:00 -0300

    Load user data on profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 import { Container, Form, Avatar } from "./styles";
 import { FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 import { BackButton } from "../../Components/BackButton";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 
+import { useAuth } from '../../hooks/auth'
 
 export function Profile() {
+  const { user } = useAuth()
+
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [oldPassword, setOldPassword] = useState("")
+  const [newPassword, setNewPassword] = useState("")
+
   return (
     <Container>
       <header>
@@ -21,10 +31,32 @@ export function Profile() {
             <input id="avatar" type="file" />
           </label>
         </Avatar>
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha atual" type="password" icon={FiLock} />
-        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+        <Input
+         placeholder="Nome"
+         type="text"
+         icon={FiUser}
+         value={name}
+         onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+         placeholder="E-mail"
+         type="text"
+         icon={FiMail}
+         value={email}
+         onChange={(e) => setEmail(e.target.value)} 
+        />
+        <Input
+         placeholder="Senha atual"
+         type="password"
+         icon={FiLock}
+         onChange={(e) => setOldPassword(e.target.value)} 
+        />
+        <Input
+         placeholder="Nova senha"
+         type="password"
+         icon={FiLock}
+         onChange={(e) => setNewPassword(e.target.value)} 
+        />
 
         <Button title="Salvar" />
       </Form>
